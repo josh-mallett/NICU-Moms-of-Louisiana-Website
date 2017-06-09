e8ddc89205f885a1f9a77be8c9c67460f031bb53
Josh Mallett <playgroundinfinity@gmail.com>
2017-06-09 00:34:32 -0500

    Update index.php

--- src/php/index.php
+++ src/php/index.php
@@ -3,19 +3,24 @@
       if ($_SERVER['REQUEST_METHOD'] == 'POST')
       {
 
+        // Declare JSON content type
         header("content-type: application/json");
         require 'validation.php';
-
+        // Initialize array to store incoming data
         $values = array();
 
+        // For each incoming piece of data that comes with the POST request
+        // in the form of variable = value
         foreach($_POST as $key => $value)
         {
+          // Call validation method from validation.php
+          // If the value is valid, then assign a variable to it and store it in
+          // the values array.
           if (test_input($value) != "Error")
           {
             $$key = test_input($value);
             $values[] = $value;
           }
-
           else
           {
             echo json_encode("Failure!");
@@ -23,17 +28,15 @@
           }
         }
 
+        // Applies email validation filter to the variable that contains the email address.
         if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+
           require 'database.php';
 
+          // Call to function from database.php file to insert array of valuesinto the MySQL Database.
           $dbInsertion = insertIntoDatabase($values);
 
-          if ($dbInsertion == false)
-          {
-            echo json_encode("Failure!");
-            exit();
-          }
-
+          // Format strings from the form data to send to nicumoms email.
           $email_header = $first_name . " " . $last_name . " has submitted a contact form message on the NICU Moms of Louisiana website!";
           $destination_email = "nicumomsofla@gmail.com";
           $email_body = "Name: " . $first_name . " " . $last_name .
@@ -41,25 +44,26 @@
                         "\nSubject: " . $subject .
                         "\n\nMessage:\n\n" . "     " . $message;
 
+          // If mail is sent successfully, return success message.
           if ( mail ($destination_email, $email_header, $email_body) )
           {
             echo json_encode("Success!");
             exit();
           }
-
+          // Else return Failure message.
           else
           {
             echo json_encode("Failure!");
             exit();
           }
         }
-
+        
+        // If email is not valid then return Failure message.
         else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
           echo json_encode("Failure!");
           exit();
         }
       }
 
+    ?>
 
-
-     ?>
